5d4f09f5c6088939afb10e254cd796596dc38c43
Goto Hayato <habita.gh@gmail.com>
2019-09-10 09:02:46 +0900

    Issue #3: Refacotring: Replace `cash-dom` with `vue` in popup function.

--- src/popup.js
+++ src/popup.js
@@ -1,14 +1,9 @@
-import cash from 'cash-dom';
+import Vue from 'vue/dist/vue.esm.js';
 import popupStyle from './sass/popup.scss';
 
-(function ($) {
+(function () {
 
 'use strict';
-
-const EXTENSION_ID = 'extension-id';
-const EXTENSION_NAME = 'extension-name';
-const SELECTOR_LISTS = '.extensions';
-const SELECTOR_SEARCH = '#search';
 
 init();
 
@@ -16,37 +11,56 @@ init();
  * Initializes the extension.
  */
 function init() {
-  insert_extensions_to_lists();
-  add_event_listeners();
-}
+  const app = new Vue({
+    el: '#app',
+    data: {
+      enabledExtensions: [],
+      disabledExtensions: [],
+      search: '',
+    },
+    async created() {
+      this.refresh();
+    },
+    methods: {
+      async refresh() {
+        const extensions = await get_extensions();
+        this.enabledExtensions = extensions.filter(e => e.enabled);
+        this.disabledExtensions = extensions.filter(e => !e.enabled);
+      },
+      // Switch on/off a chrome extension.
+      async switchExtension(extension) {
+        await switch_app(extension);
+        this.refresh();
+      },
+      liClass(extension) {
+        const classes = [];
 
-/**
- * Add event listener.
- */
-function add_event_listeners() {
-  $(SELECTOR_LISTS).on('click', 'li', (event) => {
-    switch_extension($(event.target).data(EXTENSION_ID));
-    event.stopPropagation();
-  });
-
-  // Add incremental search function.
-  $(SELECTOR_SEARCH).on('keyup', event => {
-    const value =  event.target.value.toLowerCase();
-    const $items = $(SELECTOR_LISTS).find('li');
-
-    if (value) {
-      $items.each(function (el, index) {
-        if (!el.innerHTML.toLowerCase().match(value)) {
-          $(el).addClass('hidden');
-        } else {
-          $(el).removeClass('hidden');
+        if (this.search.length > 0) {
+          let key = this.search.toLowerCase();
+          if (extension.name.toLowerCase().match(key)) {
+          } else {
+            classes.push('hidden');
+          }
         }
-      });
-    } else {
-      $items.each(function (el, index) {
-        $(el).removeClass('hidden');
-      });
-    }
+
+        return classes;
+      },
+      liStyle(extension) {
+        const styles = {};
+
+        if (extension.icons.length > 0) {
+          let [icon] = [...extension.icons].reverse();
+          styles['backgroundImage'] = `url(${icon.url})`;
+        }
+
+        return styles;
+      },
+    },
+    filters: {
+      trim(value, length) {
+        return value.length > length ? value.slice(0, length) + '...' : value;
+      },
+    },
   });
 }
 
@@ -55,45 +69,32 @@ function add_event_listeners() {
  */
 async function get_extensions() {
   const KEY = 'extensionsExcluded';
-  const apps = await get_apps();
   const result = await get_storage(KEY);
   const excluded_ids = result[KEY] || [];
   const own_id = chrome.runtime.id;
 
-  return apps.filter(app => {
+  return (await get_apps())
+    .filter(is_target)
+    .sort(compare_name);
+
+  // Check if the app is a target.
+  function is_target(app) {
     return (
       app.type === 'extension' &&
       !excluded_ids.includes(app.id) &&
       app.id !== own_id
     );
-  });
-}
-
-/**
- * Insert extensions to the lists.
- */
-async function insert_extensions_to_lists() {
-  const apps = await get_extensions();
-  const apps_enabled = [];
-  const apps_disabled = [];
-
-  apps.forEach(app => {
-    let $item = gen_list_item(app);
-
-    if (app.enabled) {
-      apps_enabled.push($item);
-    } else {
-      apps_disabled.push($item);
-    }
-  });
-
-  if (apps_enabled.length) {
-    get_target_list(true)
-      .append(apps_enabled.sort(data_comparator(EXTENSION_NAME)));
   }
-  if (apps_disabled.length) {
-    get_target_list(false)
-      .append(apps_disabled.sort(data_comparator(EXTENSION_NAME)));
+
+  // Comparator which uses `.name` property.
+  function compare_name(e1, e2) {
+    if (e1.name < e2.name) {
+      return -1;
+    } else if (e1.name > e2.name) {
+      return 1;
+    }
+
+    return 0;
   }
 }
 
@@ -120,58 +121,11 @@ async function get_storage(key) {
 }
 
 /**
- * Generates a list item for an extension.
- */
-function gen_list_item(app) {
-  const name = (app.name.length > 30) ? app.name.slice(0, 30) + "..." :  app.name;
-  const $el = $('<li>');
-
-  $el.data(EXTENSION_ID, app.id)
-    .data(EXTENSION_NAME, app.name)
-    .text(name);
-  if (app.icons) {
-    const icon = app.icons[app.icons.length - 1].url
-    $el.css('background-image', `url(${icon})`);
-  }
-
-  return $el;
-}
-
-/**
- * Moves the extension li to the new list.
- */
-function move_extension_in_list(app) {
-  const $el = $(SELECTOR_LISTS).find('li');
-
-  // The argument is different from one in jQuery.
-  const $appLi = $el.filter(function (el) {
-    return $(el).data(EXTENSION_ID) === app.id;
-  });
-
-  const $list = get_target_list(app.enabled);
-
-  $appLi.appendTo($list);
-
-  const $items = sort($list.find('li'), data_comparator(EXTENSION_NAME));
-  $items.appendTo($list);
-}
-
-/**
- * Switches on/off a chrome extension.
- */
-async function switch_extension(id) {
-  const app = await get_app(id);
-  await switch_app(id, !app.enabled);
-  const app_updated = await get_app(id);
-  move_extension_in_list(app_updated);
-}
-
-/**
  * Get an app info.
  */
 async function get_app(id) {
   return new Promise(resolve => {
-    chrome.management.get(id, (app) => {
+    chrome.management.get(id, app => {
       resolve(app);
     });
   });
@@ -180,44 +134,12 @@ async function get_app(id) {
 /**
  * Switch the state of an app.
  */
-async function switch_app(id, enabled) {
+async function switch_app(app) {
   return new Promise(resolve => {
-    chrome.management.setEnabled(id, enabled, () => {
+    chrome.management.setEnabled(app.id, !app.enabled, () => {
       resolve();
     });
   });
 }
 
-/**
- * Gets the enabled or disabled list.
- */
-function get_target_list(enabled) {
-  const list = enabled ? $('.extensions-enabled') : $('.extensions-disabled');
-  return list.first();
-}
-
-/**
- * Helper function: Sorts an items list.
- */
-function sort($items, comparator) {
-  return Array.prototype.sort.call($items, comparator);
-}
-
-/**
- * Helper function: Generates a comparison function with data() for sort.
- */
-function data_comparator(key) {
-  return (a, b) => {
-    const $a = $(a);
-    const $b = $(b);
-
-    if ($a.data(key) < $b.data(key)) {
-      return -1;
-    } else if ($a.data(key) > $b.data(key)) {
-      return 1;
-    }
-    return 0;
-  };
-}
-
-})(cash);
+})();
